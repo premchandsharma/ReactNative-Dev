0340ed12cff03fe35db994c47448a774212ce937
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2025-09-04 11:32:21 +0100

    feat: add types for campaign tooltips and their properties
    Signed-off-by: Jeffery Orazulike <chukwudumebiorazulike@gmail.com>

---
+++ src/domain/tooltips/types.ts
@@ -0,0 +1,48 @@
+export type CampaignTooltips = {
+  id: string;
+  campaign_type: 'TTP';
+  details: {
+    _id: string;
+    campaign: string;
+    created_at: string;
+    name: string;
+    screenId: string;
+    tooltips: Array<Tooltip>;
+  };
+};
+
+export type Tooltip = {
+  _id: string;
+  clickAction: string;
+  deepLinkUrl: string;
+  eventName: string;
+  link: string;
+  order: number;
+  position: string;
+  styling: {
+    backgroundColor: string;
+    closeButton: boolean;
+    enableBackdrop: boolean;
+    highlightPadding: string;
+    highlightRadius: string;
+    spacing: {
+      paddingBottom: string;
+      paddingLeft: string;
+      paddingRight: string;
+      paddingTop: string;
+    };
+    tooltipArrow: {
+      arrowHeight: string;
+      arrowWidth: string;
+    };
+    tooltipDimensions: {
+      cornerRadius: string;
+      height: string;
+      width: string;
+    };
+  };
+  target: string;
+  triggerType: string;
+  type: string;
+  url: string;
+}
